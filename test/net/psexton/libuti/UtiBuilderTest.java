af7ee56bf488a86f04137672328557fd1723eaf2
Paul Sexton <psexton@gnresound.com>
2015-08-27 14:53:06 -0500

    test(UtiBuilder): add unit tests for fromFile

--- test/net/psexton/libuti/UtiBuilderTest.java
+++ test/net/psexton/libuti/UtiBuilderTest.java
@@ -4,15 +4,22 @@
  */
 package net.psexton.libuti;
 
+import java.io.File;
+import java.io.IOException;
 import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
 
 /**
  *
  * @author PSexton
  */
 public class UtiBuilderTest {
+    
+     @Rule
+    public TemporaryFolder temp = new TemporaryFolder();
     
     /**
      * Test of fromFile method, of class UtiBuilder.
@@ -74,4 +81,21 @@ public class UtiBuilderTest {
         assertEquals(expResult, result);
     }
     
+    @Test
+    public void testFromFileMatches() throws IOException {
+        File file = temp.newFile("foo.txt");
+        UtiBuilder instance = new UtiBuilder();
+        Uti expResult = new Uti("public.plain-text");
+        Uti result = instance.fromFile(file);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testFromFileDoesNotMatch() throws IOException {
+        File file = temp.newFile("foo.notevenclosetoarealextension");
+        UtiBuilder instance = new UtiBuilder();
+        Uti expResult = new Uti("dyn.notevenclosetoarealextension");
+        Uti result = instance.fromFile(file);
+        assertEquals(expResult, result);
+    }
 }
