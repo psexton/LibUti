47220f093e10f0446c840a3dd6cc391c86565620
Paul Sexton <paul@psexton.net>
2018-04-07 19:51:23 -0500

    Switched to maven (mostly)

--- src/main/java/net/psexton/libuti/UtiDb.java
+++ src/main/java/net/psexton/libuti/UtiDb.java
@@ -28,10 +28,10 @@ import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.input.SAXBuilder;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
 /**
  * Database for UTIs
